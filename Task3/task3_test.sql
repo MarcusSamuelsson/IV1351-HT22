f489b627037c9e23502fcdb2f3976eb05eb85d81
Marcus Samuelsson <Samuelsson.marcus17@gmail.com>
2022-12-05 14:23:41 +0100

    fix

--- Task3/task3_test.sql
+++ Task3/task3_test.sql
@@ -1,6 +1,6 @@
 CREATE VIEW lesson_per_month AS
     SELECT count(EXTRACT(month FROM time)) as month, 
-    count(lesson_type) FILTER (WHERE lesson_type = 'induvidual') as induvidual_lesson, 
+    count(lesson_type) FILTER (WHERE lesson_type = 'induvidual') as individual_lesson, 
     count(lesson_type) FILTER (WHERE lesson_type = 'group') as group_lesson, 
     count(lesson_type) FILTER (WHERE lesson_type = 'ensemble') as ensemble 
     FROM lesson WHERE EXTRACT(year FROM time) = '2021' GROUP BY EXTRACT(MONTH FROM time) ORDER BY EXTRACT(MONTH FROM time) ASC;
@@ -12,12 +12,19 @@ CREATE VIEW student_nr_of_siblings
         GROUP BY student.student_id) as nr_of_siblings 
     GROUP BY nr_of_siblings ORDER BY nr_of_siblings ASC;
 
-CREATE VIEW instructor_holding_lessons
+    SELECT nr_of_siblings, count(1) as students_with_nr_of_siblings FROM (SELECT count(sibling.student_id) FROM student LEFT JOIN sibling ON student.student_id = sibling.student_id  GROUP BY student.student_id) as nr_of_siblings GROUP BY nr_of_siblings ORDER BY nr_of_siblings ASC;
+
+CREATE VIEW student_nr_of_siblings
     SELECT lesson.instructor_id, count(lesson.instructor_id) 
     FROM lesson LEFT JOIN instructor ON instructor.instructor_id = lesson.instructor_id 
     WHERE EXTRACT(month FROM time) = EXTRACT(month FROM now()) AND EXTRACT(year FROM time) = EXTRACT(year FROM now()) 
     GROUP BY lesson.instructor_id HAVING count(lesson.instructor_id) > 1;
+
     
+    SELECT lesson.instructor_id, count(lesson.instructor_id) FROM lesson LEFT JOIN instructor ON instructor.instructor_id = lesson.instructor_id WHERE EXTRACT(month FROM time) = EXTRACT(month FROM now()) AND EXTRACT(year FROM time) = EXTRACT(year FROM now()) GROUP BY lesson.instructor_id HAVING count(lesson.instructor_id) > 1;
+    
+    SELECT lesson.instructor_id, count(lesson.instructor_id) FROM lesson LEFT JOIN instructor ON instructor.instructor_id = lesson.instructor_id WHERE EXTRACT(month FROM time) = '10' AND EXTRACT(year FROM time) = '2021' GROUP BY lesson.instructor_id HAVING count(lesson.instructor_id) > 1;
+
 
 CREATE MATERIALIZED VIEW upcoming_ensembles AS
     SELECT lesson_type, genre, time, 
